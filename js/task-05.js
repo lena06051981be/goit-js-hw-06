8d66d83ac541d48f545b71b442a7054299c3d95c
Maksym Solomko <lena06051981be@gmail.com>
2022-09-15 22:15:14 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const inputRef = document.querySelector('#name-input');
+const spanNameRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', () => {
+    if (inputRef.value.length === 0) {
+       return spanNameRef.textContent = 'Anonymous';
+    }
+    spanNameRef.textContent = inputRef.value;
+});
