a096f78d4bec785f225372056c2f577454fe04c4
Maksym Solomko <lena06051981be@gmail.com>
2022-09-17 20:47:59 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,22 @@
-const categoryItemsRef = document.querySelectorAll('#categories .item');
-console.log("Number of categories: ", categoryItemsRef.length);
+const numberOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", numberOfCategories.length);
 
-const categoryTitlesRef = document.querySelectorAll('.item > h2');
+const titleH1 = document.querySelector("#categories");
 
-categoryTitlesRef.forEach(item => {
-    console.log(`Category: ${item.textContent}`);
-    console.log(`Elements: ${item.nextElementSibling.children.length}`);
-})
+const result = numberOfCategories.forEach((elem) => {
+    const title = elem.querySelector("h2").textContent;
+    const number = elem.querySelectorAll("li").length;
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${number}`);
+    return titleH1.append(`${title}: ${number}  `);
+});
+
+// ну или так, без вывода на страницу)
+
+// const numberOfCategories = document.querySelectorAll(".item");
+// console.log("Number of categories:", numberOfCategories.length);
+
+// const categoryNamesAndCounts = numberOfCategories.forEach((elem) => {
+//   console.log(Category: ${elem.querySelector("h2").textContent});
+//   console.log(Elements: ${elem.querySelectorAll("li").length});
+// });
