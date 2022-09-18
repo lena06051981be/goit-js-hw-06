22d242606f8931c288907afed45ee24090e98c64
Maksym Solomko <lena06051981be@gmail.com>
2022-09-18 19:49:50 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,13 @@
+const btnDecrRef = document.querySelector(`button[data-action="decrement"]`);
+const btnIncrRef = document.querySelector(`button[data-action="increment"]`);
+const counterRef = document.querySelector("#value");
+
 let counterValue = 0;
 
-const decrementBtnRef = document.querySelector('[data-action="decrement"]');
-const incrementBtnRef = document.querySelector('[data-action="increment"]');
-const valueRef = document.querySelector('#value');
+btnDecrRef.addEventListener("click", () => {
+  counterRef.textContent = `${(counterValue -= 1)}`;
+});
 
-const increaseValue = () => {
-    counterValue += 1;
-    valueRef.textContent = counterValue;    
-}
-
-const decreaseValue = () => {
-    counterValue -= 1;
-    valueRef.textContent = counterValue;    
-}
-
-decrementBtnRef.addEventListener('click', decreaseValue);
-incrementBtnRef.addEventListener('click', increaseValue);
+btnIncrRef.addEventListener("click", () => {
+  counterRef.textContent = `${(counterValue += 1)}`;
+});
